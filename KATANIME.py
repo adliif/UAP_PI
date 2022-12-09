228f6bf1643222918478bd6bb1ed4ac3f9cd0bda
TASYA AZZAHRA PUTRI (2117051043) <92432388+tasyaazzahraputri@users.noreply.github.com>
2022-12-09 10:00:29 +0700

    Update KATANIME.py

--- KATANIME.py
+++ KATANIME.py
@@ -87,6 +87,15 @@ def main():
           '\nCharacter\t:', i['character'], '\nAnime\t\t:', i['anime'], '\n')
 
         simpan = input('Buat file notepad untuk menyalin quotes? [Y/N] : ')
+        
+          if simpan == 'Y':
+            fa = open('Katanime.txt', 'a')                                                    # File handling
+            fa.write('')
+            print('Notepad Anda berhasil dibuat')
+          elif simpan == 'N':
+            print('Terima kasih telah berkunjung :)')
+          else:
+            print('Masukan Anda tidak tersedia')
 
                 
       elif pilih == 2:
